3c34fb0f3302e18ff230f0c277f3af9c1cf79209
freeman14 <dubovetzky@me.com>
2014-12-12 18:09:31 +0200

    FIx

--- js/script.js
+++ js/script.js
@@ -6,16 +6,16 @@ var filtersMenuOpen = false;
 filtersBtn.button(function(e){
     e.preventDefault();
     if(filtersMenuOpen){
-        layout.removeClass('menu-open');
+        Velocity(HS().sClass('layout'), { left: 0 }, { duration: 100 });
         offCanvasMenu.removeClass('menu-open').css('width', '');
-        Velocity(HS().sClass('layout'), { left: 0 });
         filtersMenuOpen = false;
+
     } else {
         var hotelPanelWidth = HS().findOne('.hotel').css('width');
         console.log(hotelPanelWidth);
-        layout.addClass('menu-open');
-        offCanvasMenu.css('width', hotelPanelWidth).addClass('menu-open');
-        Velocity(HS().sClass('layout'), { left: hotelPanelWidth });
+        //layout.addClass('menu-open');
+        offCanvasMenu.addClass('menu-open').css('width', hotelPanelWidth);
+        Velocity(HS().sClass('layout'), { left: hotelPanelWidth }, { duration: 100 });
         filtersMenuOpen = true;
     }
 });
